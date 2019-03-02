1303d4b126d790891e54e7ee1d587d4c19f94283
Lu <luguodata@gmail.com>
2019-03-02 16:14:21 -0500

    Finished ETL and Model Training Scripts

--- app/run.py
+++ app/run.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 
 from flask import Flask
 from flask import render_template, request, jsonify
@@ -14,6 +15,7 @@ from sqlalchemy import create_engine
 
 app = Flask(__name__)
 
+"""
 def tokenize(text):
     tokens = word_tokenize(text)
     lemmatizer = WordNetLemmatizer()
@@ -24,25 +26,51 @@ def tokenize(text):
         clean_tokens.append(clean_tok)
 
     return clean_tokens
+"""
+
+
+def tokenize(text):
+    """Nomalize, tokenize and lemmatize process for text
+
+    Args:
+        text (str)
+
+    Return:
+        text_lems: list of element of the text after normalization, tokenization
+        stopwords removal and lemmetization
+    """
+    # Normailization -- lower case + remove puntuation
+    text = re.sub(r"[^a-zA-Z0-9]"," ",text.lower())
+
+    # tokenization
+    tokens = word_tokenize(text)
+
+    # Remove stop words
+    words = [word for word in tokens if word not in stopwords.words("english")]
+
+    # stemmization
+    text_lems = [WordNetLemmatizer().lemmatize(lem).strip() for lem in words]
+
+    return text_lems
 
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+engine = create_engine('sqlite:///data/DisasterResponse.db')
+df = pd.read_sql_table('ETL_processed_data', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("/models/RF_CV.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
 @app.route('/')
 @app.route('/index')
 def index():
-    
+
     # extract data needed for visuals
     # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
-    
+
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
     graphs = [
@@ -64,29 +92,30 @@ def index():
                 }
             }
         }
+
     ]
-    
+
     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
-    
+
     # render web page with plotly graphs
-    return render_template('master.html', ids=ids, graphJSON=graphJSON)
+    return render_template('templates/master.html', ids=ids, graphJSON=graphJSON)
 
 
 # web page that handles user query and displays model results
 @app.route('/go')
 def go():
     # save user input in query
-    query = request.args.get('query', '') 
+    query = request.args.get('query', '')
 
     # use model to predict classification for query
     classification_labels = model.predict([query])[0]
     classification_results = dict(zip(df.columns[4:], classification_labels))
 
-    # This will render the go.html Please see that file. 
+    # This will render the go.html Please see that file.
     return render_template(
-        'go.html',
+        'templates/go.html',
         query=query,
         classification_result=classification_results
     )
---
+++ data/process_data.py
@@ -0,0 +1,90 @@
+import sys
+import pandas as pd
+from sqlalchemy import create_engine
+
+def load_data(messages_filepath, categories_filepath):
+    """ Load message data and categories datasets for DisasterResponse project
+
+    Args:
+        messages_filepath(str): file path of message data
+        categories_filepath(str): file path of categories data
+
+    Returns:
+        df: Merged dataset of message and categories
+    """
+    messages = pd.read_csv(messages_filepath, sep = ',')
+    categories = pd.read_csv(categories_filepath, sep = ',')
+    df = messages.merge(categories, on = ['id'], how = 'left')
+
+    return df
+
+def clean_data(df):
+    """ Clean merge data of message and categories. This step includes
+        categories splits, converting categories into dummy values, drop
+        duplicated records.
+
+    Args:
+        df: merged data from message and categories
+
+    Returns:
+        df: cleaned df
+    """
+    categorie_seg = df['categories'].str.split(';', expand = True)
+    categorie_seg.columns = categorie_seg.head(1).values[0].tolist()
+
+    for column in categorie_seg:
+        # set each value to be the last character of the string
+        categorie_seg[column] = categorie_seg[column].str[-1]
+
+        # convert column from string to numeric
+        categorie_seg[column] = categorie_seg[column].astype(int)
+
+    df = df.drop(['categories'], axis = 1)
+    df = pd.concat([df, categorie_seg], axis = 1)
+    df = df.drop_duplicates()
+
+    return df
+
+def save_data(df, database_filename):
+    """Create sqlite database and save the processed data into it.
+
+    Args:
+        df: cleaned merging data of message and categories
+        database_filename: sqlite database name
+
+    Return:
+        None
+    """
+
+    engine = create_engine('sqlite:///{}'.format(database_filename))
+    df.to_sql('ETL_processed_data', engine, index=False)
+
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+
+        print('Cleaned data saved to database!')
+
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
