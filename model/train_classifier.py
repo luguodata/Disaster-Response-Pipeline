14bb60806441f481c81e93e78695b108dd6aedef
Lu <luguodata@gmail.com>
2019-03-05 22:50:46 -0500

    Updated grid search parameters and got better prediction performance

--- model/train_classifier.py
+++ model/train_classifier.py
@@ -199,9 +199,9 @@ def build_model():
 
     # grid search
     parameters = {
-        'clf__estimator__n_estimators': [20, 50],
+        #'clf__estimator__n_estimators': [10,20],
         #'clf__estimator__max_depth': [4, 8],
-        'clf__estimator__min_samples_split': [2,4]
+        'clf__estimator__min_samples_split': [3,6]
     }
 
     cv = GridSearchCV(pipeline, param_grid= parameters)
